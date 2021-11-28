92f55269351b62f8b99c0c628f8961d25c2dc596
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-28 21:59:45 +0000

    new page

---
+++ src/pages/something.tsx
@@ -0,0 +1,14 @@
+import {NextPage} from "next";
+import React from "react";
+
+const Something: NextPage = () => {
+
+    return (
+        <div>
+            <h1>Something</h1>
+            <p>something...</p>
+        </div>
+    );
+}
+
+export default Something;
