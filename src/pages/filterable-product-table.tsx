a03f5eef70a17e296cfa13bbabd788e4d1e81573
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-28 22:23:58 +0000

    more work

--- src/pages/filterable-product-table.tsx
+++ src/pages/filterable-product-table.tsx
@@ -1,5 +1,5 @@
 import {NextPage} from "next";
-import React from "react";
+import React, {ReactChild} from "react";
 
 type Product = {
     category: string,
@@ -9,12 +9,12 @@ type Product = {
 }
 
 const PRODUCTS: Product[] = [
-    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+    {category: 'Sporting Goods', price: '£49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '£19.99', stocked: true, name: 'Badminton Racket'},
+    {category: 'Sporting Goods', price: '£29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '£99.99', stocked: true, name: 'Laptop'},
+    {category: 'Electronics', price: '£399.99', stocked: false, name: 'iPhone 13'},
+    {category: 'Electronics', price: '£199.99', stocked: true, name: 'Pixel 5'}
 ];
 
 const FilterableProductTable: React.FC = () => {
@@ -29,16 +29,39 @@ const FilterableProductTable: React.FC = () => {
 const SearchBar: React.FC = () => {
     return (
         <form>
-            <input type={"text"} placeholder={"Search for products"}/>
+            <input type={"text"} placeholder={"Search for products"} />
+            <br/>
+            <p>
+                <input type={"checkbox"} />
+                {" "}
+                Hide out-of-stock items.
+            </p>
         </form>
     );
 };
 
 const ProductTable: React.FC<{products: Product[]}> = ({products}) => {
-    return (
-        <div>
 
-        </div>
+    const productRows: ReactChild[] = [];
+
+    products.forEach((product, index) => {
+        productRows.push(
+            <ProductRow product={product} key={product.name}/>
+        );
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {productRows}
+            </tbody>
+        </table>
     );
 };
 
@@ -50,16 +73,16 @@ const ProductCategoryRow: React.FC = () => {
     );
 };
 
-const ProductRow: React.FC = () => {
+const ProductRow: React.FC<{product: Product}> = ({product}) => {
     return (
-        <div>
-
-        </div>
+        <tr>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+        </tr>
     );
 };
 
 const FilterableProductTablePage: NextPage = () => {
-
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
             <h1>Filterable Product Table</h1>
